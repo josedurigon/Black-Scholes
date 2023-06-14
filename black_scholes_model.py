eb60bcd85a68217d2ef3fe94503b7b8e666dc19d
josedurigon <68879294+josedurigon@users.noreply.github.com>
2023-06-14 01:40:12 -0300

    Update black_scholes_model.py
    Update

--- black_scholes_model.py
+++ black_scholes_model.py
@@ -1,26 +1,58 @@
-#implement black and scholes formula
+#implementation of black and scholes formula
 import numpy as np
-from scipy.stats import norm
 
-#variables
-r = 0.1375
-s = 30
-k = 40
-t = 240/365
-sigma = 0.30
+# Variables
+r = float(input("Free risk rate: "))
+s = float(input("Subjacent equity price: "))
+k = float(input("Option strike price: "))
+t = float(input("Time till default in days (just put the number please thx): "))
+t = t / 365
+sigma = float(input("Volatility (that you should have calculated mate): "))
+option_type = input("CALL[C] or PUT[P]: ").upper()
 
-def blackScholes(r,s,k,t,sigma, type="C"):
-    d1=(np.log(s/k)+(r+sigma**2/2)*t)/(sigma*np.sqrt(t))
-    d2= d1 - sigma *np.sqrt(t)
+def cumulative_normal_distribution(x):
+    return (1.0 + np.math.erf(x / np.sqrt(2.0))) / 2.0
+
+def calculate_implied_volatility(r, s, k, t, price, option_type="C"):
+    # Implementar o cálculo da volatilidade implícita
+    # usando métodos iterativos, como o método de Newton ou o método de bisseção.
+    # Retornar a volatilidade implícita calculada
+    pass
+
+def calculate_delta(r, s, k, t, sigma, option_type="C"):
+    d1 = (np.log(s / k) + (r + sigma ** 2 / 2) * t) / (sigma * np.sqrt(t))
+
+    if option_type == "C":
+        delta = cumulative_normal_distribution(d1)
+    elif option_type == "P":
+        delta = cumulative_normal_distribution(d1) - 1
+
+    return delta
+
+def calculate_gama(r, s, k, t, sigma):
+    d1 = (np.log(s / k) + (r + sigma ** 2 / 2) * t) / (sigma * np.sqrt(t))
+    gama = cumulative_normal_distribution(d1) / (s * sigma * np.sqrt(t))
+    return gama
+
+def calculate_vega(r, s, k, t, sigma):
+    d1 = (np.log(s / k) + (r + sigma ** 2 / 2) * t) / (sigma * np.sqrt(t))
+    vega = s * np.sqrt(t) * np.exp(-d1 ** 2 / 2) / np.sqrt(2 * np.pi)
+    return vega
+
+
+def blackScholes(r, s, k, t, sigma, option_type="C"):
+    d1 = (np.log(s / k) + (r + sigma ** 2 / 2) * t) / (sigma * np.sqrt(t))
+    d2 = d1 - sigma * np.sqrt(t)
 
     try:
-        if type =="C":
-            price = s*norm.cdf(d1,0,1)- k*np.exp(-r*t)*norm.cdf(d2,0,1)
-
-        elif type=="C":
-            price = k*np.exp(-r*t)*norm.cdf(-d2,0,1)-s*norm.cdf(-d1,0,1)
+        if option_type == "C":
+            price = s * cumulative_normal_distribution(d1) - k * np.exp(-r * t) * cumulative_normal_distribution(d2)
+        elif option_type == "P":
+            price = k * np.exp(-r * t) * cumulative_normal_distribution(-d2) - s * cumulative_normal_distribution(-d1)
         return price
     except:
         print("Please confirm all options parameters above...")
-    
-print("Option Price is: ", round(blackScholes(r,s,k,t,sigma, type="C"),2))
+
+option_price = blackScholes(r, s, k, t, sigma, option_type=option_type)
+print("Option Price is:", round(option_price, 2))
+
